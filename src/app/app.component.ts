c98ecbdf277730c49b8e6aaa8bf1e10b95cce857
Ul1ra <benpcunningham@outlook.com>
2018-07-03 18:22:08 +0200

    basic frontend list of airtable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  participants: Array<any>;
+
+  constructor(private http: HttpClient) {
+    http
+      .get('http://localhost:3000/participants?maxRecords=3&view=Grid%20view')
+      .subscribe((result: any) => this.participants = result.records);
+  }
 }
